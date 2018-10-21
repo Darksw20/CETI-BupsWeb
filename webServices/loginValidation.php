ae72ccdceeff04acbef504b01962ba6f259f7415
Darksw20 <ricardog.navi20@gmail.com>
2018-10-21 16:00:41 -0500

    Primera actualizacion de movil

---
+++ webServices/loginValidation.php
@@ -0,0 +1,41 @@
+<?php 
+	
+	$db_hostname="localhost";
+	$db_database="bups";
+	$db_username="root";
+	$db_password="";
+
+	$json=array();
+
+	if(isset($_POST["CUM"])&&isset($_POST["Pass"]))
+	{
+		$CUM=$_POST['CUM'];
+		$password=$_POST['Pass'];
+
+		
+		$conexion=mysqli_connect($db_hostname,$db_username,$db_password,$db_database);
+
+		/*
+			Aqui se hace un script para revisar que existan los componentes necesarios
+			para poder entrar de manera correcta al sistema
+		*/
+		$query="SELECT Tipo_Usuario FROM usuario WHERE CUM='".$CUM."'AND Password='".$password."'";
+		$res_query=mysqli_query($conexion,$query);
+
+		if($registro=mysqli_fetch_array($res_query))
+		{
+			$json['tipo']=$registro['Tipo_Usuario'];
+		}
+		else
+		{
+			$json['tipo']=0;
+		}
+		mysqli_close($conexion);
+		echo json_encode($json);
+	}
+	else
+	{
+		$json['tipo']=404;
+		echo json_encode($json);
+	}
+?>
---
+++ webServices/userNameAvailable.php
@@ -0,0 +1,47 @@
+<?php 
+
+	$db_hostname='localhost';
+	$db_database='BuPs';
+	$db_username='root';
+	$db_password='';
+	$json=array();
+
+	if (isset($_POST['CUM'])) 
+	{
+		$CUM=$_POST["CUM"];
+		
+		//$CUM="JAL0720230";
+		
+		$conexion=mysqli_connect($db_hostname,$db_username,$db_password,$db_database);
+		if($conexion)
+		{
+			$query="SELECT * FROM usuario WHERE CUM='".$CUM."'";
+			$res_query=mysqli_query($conexion,$query);
+			if($valid=mysqli_fetch_array($res_query))
+			{
+
+				$json['validado']=true;
+
+			}
+			else
+			{
+				$json['validado']=false;
+			}
+		}
+			/*
+				Aqui se hace un script para revisar que existan el usuario ingresado
+			*/
+			
+			mysqli_close($conexion);
+			//print "[";
+			echo json_encode($json);
+			//print "]";
+	}
+	else
+	{
+
+		$json['validado']=false;
+		//echo json_encode($json);
+	}
+
+	?>
